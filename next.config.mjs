012887b7f1b1123b67536eb8868ffdb6a8e90a86
xuke <xk1242425209@163.com>
2024-12-25 14:53:16 +0800

    Remove unused redirects configuration from next.config.mjs and add experimental field

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  /**
-   * 通过 next.config.js 文件中的 redirects 选项，
-   * 可以将输入的请求路径重定向到不同的目标路径。
-   * @see https://nextjs.org/docs/app/building-your-application/routing/redirecting#redirects-in-nextconfigjs
-   */
-  async redirects() {
-    return [
-      {
-        source: '/redirect/redirect-id/:id',
-        destination: '/redirect/to-redirect-id/:id',
-        permanent: true,
-      },
-    ];
-  },
+  experimental: {},
 };
 
 export default nextConfig;
